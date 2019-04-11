3fd6672c1faeaf622d746bf3a9500fb70cb42092
Mohit Malik <32329331+mohit61@users.noreply.github.com>
2019-04-11 17:27:58 +0530

    Update book_list.js

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,6 +1,8 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
-import {selectBook} from '../actions/index';
+//import {selectBook} from '../actions/index';
+// agar export default krte ho to {selectBook} i.e. {} braces me likhne ki zaroorat nhi hai
+import selectBook from '../actions/index';
 import {bindActionCreators} from 'redux';
 
 class BookList extends Component{
